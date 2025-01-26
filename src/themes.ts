9c456cd90cbcc37641b0209c34e15376f1ac225e
0HugoHu <0@hugohu.top>
2025-01-25 23:26:11 -0800

    Feat: update the personal tool list

--- src/themes.ts
+++ src/themes.ts
@@ -16,10 +16,10 @@ export const lightThemeOverrides: GlobalThemeOverrides = {
 
 export const darkThemeOverrides: GlobalThemeOverrides = {
   common: {
-    primaryColor: '#1ea54cFF',
-    primaryColorHover: '#36AD6AFF',
-    primaryColorPressed: '#0C7A43FF',
-    primaryColorSuppl: '#36AD6AFF',
+    primaryColor: '#222831',
+    primaryColorHover: '#31363F',
+    primaryColorPressed: '#76ABAE',
+    primaryColorSuppl: '#EEEEEE',
   },
 
   Notification: {
